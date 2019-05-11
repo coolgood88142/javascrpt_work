3aa41cf6b065e7e0b4a5ce737a82d112813037c3
Kai <coolgood88142@gmail.com>
2019-05-12 02:57:29 +0800

    調整package.json

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end();
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const app = require('express')(); 
+const port = process.env.PORT || 3000; 
+app.get('/', function (req, res) { 
+    res.sendfile(__dirname + '/index.html'); 
+}); 
+app.listen(port, function() { 
+    console.log("Node app is running at localhost:" + app.get('port')) 
+}); 
